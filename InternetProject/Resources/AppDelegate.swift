718d87b7c8eb26c8d8831f8768514cf84b079e57
Jtmonkeyboy <43076688+Jtmonkeyboy@users.noreply.github.com>
2018-12-14 13:26:26 -0700

    Made my internet controllers.

--- InternetProject/Resources/AppDelegate.swift
+++ InternetProject/Resources/AppDelegate.swift
@@ -18,6 +18,10 @@ class AppDelegate: UIResponder, UIApplicationDelegate, UISplitViewControllerDele
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool
     {
         // Override point for customization after application launch.
+        let splitViewController = window!.rootViewController as! UISplitViewController
+        let navigationController = splitViewController.viewControllers[splitViewController.viewControllers.count-1] as! UINavigationController
+        navigationController.topViewController!.navigationItem.leftBarButtonItem = splitViewController.displayModeButtonItem
+        splitViewController.delegate = self
         return true
     }
 
@@ -46,6 +50,21 @@ class AppDelegate: UIResponder, UIApplicationDelegate, UISplitViewControllerDele
     func applicationWillTerminate(_ application: UIApplication)
     {
         // Called when the application is about to terminate. Save data if appropriate. See also applicationDidEnterBackground:.
+    }
+    
+    func splitViewController(_ splitViewControoller: UISplitViewController, collapseSecondary secondaryViewController:UIViewController, onto primaryViewController:UIViewController) -> Bool
+    {
+        guard let secondaryAsNavController = secondaryViewController as? UINavigationController
+        else
+        {
+            return false
+        }
+        guard let topAsDetailController = secondaryAsNavController.topViewController as? InternetDetailViewController
+        else
+        {
+            return false
+        }
+        return false
     }
 
 
